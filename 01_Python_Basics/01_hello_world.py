99155bfe4ed53e3d2ebb712adfa60102dbca5068
wardf1113 <112917311+wardf1113@users.noreply.github.com>
2022-09-14 13:14:30 +1200

    Add files via upload

---
+++ 01_Python_Basics/01_hello_world.py
@@ -0,0 +1,1 @@
+print("01_hello_world.py")
---
+++ 01_Python_Basics/02_variables.py
@@ -0,0 +1,11 @@
+# code to learn about variables
+
+# variables can be string
+greetings = "hello world"
+
+# they can also be numbers
+fav_num = 7
+print(fav_num * 2)
+
+num_string = "5"
+print(num_string * 5)
