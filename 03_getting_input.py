4d445b86287dbc4ee64d7d8a275385e6f69e5952
wardf1113 <112917311+wardf1113@users.noreply.github.com>
2022-09-22 11:54:47 +1200

    Add files via upload

---
+++ 03_getting_input.py
@@ -0,0 +1,21 @@
+# Ask the user for their name
+username = input("What is your name ")
+
+# Ask the user for their favourite interger
+fav_num = int(input("Favourite Number?  "))
+
+# Double, halve and square the number
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
+
+print()
+# Greet the user
+print("Hi {}, your favourite number is {}".format(username, fav_num))
+print()
+# Output the results of doubling, halving
+# and squaring their favourite number
+print("double {} is {}".format(fav_num, double))
+print("half {} is {}".format(fav_num, half))
+print("{} squared is {}".format(fav_num, squared))
+print()
